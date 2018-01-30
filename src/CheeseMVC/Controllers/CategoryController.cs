eb4be1aedf63fa5768d081b6e152030a0ab46a38
Victor Tran <Victor.n.tran@outlook.com>
2018-01-29 20:50:37 -0500

    Created and Index view for categories to be displayed and passed in a list of categories

--- src/CheeseMVC/Controllers/CategoryController.cs
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CheeseMVC.Data;
+using CheeseMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -20,7 +21,8 @@ namespace CheeseMVC.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            IList<CheeseCategory> Categories = context.Categories.ToList();
+            return View(Categories);
         }
     }
 }
