563f9749076e5d31ca76e29f37478362a0a61d6e
Victor Tran <Victor.n.tran@outlook.com>
2018-01-31 00:26:54 -0500

    Changed namespace to accurately reflect location of AddMenuViewModel

--- src/CheeseMVC/ViewModels/AddMenuViewModel.cs
+++ src/CheeseMVC/ViewModels/AddMenuViewModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CheeseMVC.Views.Menu
+namespace CheeseMVC.ViewModels
 {
     public class AddMenuViewModel
     {
