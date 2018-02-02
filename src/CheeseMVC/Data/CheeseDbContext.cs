b0d1c847da2a509034ce0e87bc6fc70c68c0e19c
Victor <victor.n.tran@outlook.com>
2018-02-02 13:58:56 -0500

    Moved some code around, nothing new added.

--- src/CheeseMVC/Data/CheeseDbContext.cs
+++ src/CheeseMVC/Data/CheeseDbContext.cs
@@ -7,22 +7,21 @@ namespace CheeseMVC.Data
     public class CheeseDbContext : DbContext
     {
         public DbSet<Cheese> Cheeses { get; set; }
-        //EF will create a table called Categories
         public DbSet<CheeseCategory> Categories { get; set; }
-
         public DbSet<Menu> Menus { get; set; }
         public DbSet<CheeseMenu> CheeseMenus { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<CheeseMenu>().HasKey(c => new { c.MenuID, c.CheeseID });
-        }
-
+        
         public List<Cheese> Include { get; internal set; }
 
-        public CheeseDbContext(DbContextOptions<CheeseDbContext> options) 
-            : base(options)
+        public CheeseDbContext(DbContextOptions<CheeseDbContext> options) : base(options)
         { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CheeseMenu>()
+                .HasKey(c => new { c.CheeseID, c.MenuID });
+        }
 
     }
 }
